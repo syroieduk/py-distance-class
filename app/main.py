83c0354f242a351cb247be3d30af3122519e14d8
Yurii Syroied <syroiedyur@gmail.com>
2026-08-12 03:30:33 +0300

    Task for Methods FixedFixed

--- app/main.py
+++ app/main.py
@@ -19,6 +19,7 @@ class Distance:
     def __iadd__(self, other: "int | float | Distance") -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
+            return self
 
         self.km += other
         return self
